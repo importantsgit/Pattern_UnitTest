72f7eff9f334c748fe0ea65ab44b25d57017b1cd
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-04-24 11:21:58 +0900

    Initial Commit

---
+++ PatternStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatternStudy
+//
+//  Created by 이재훈 on 2023/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
